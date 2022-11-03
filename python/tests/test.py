32cc2362559d0dca5c6f55cbfeecf207f35cb380
Saul Shanabrook <s.shanabrook@gmail.com>
2022-11-03 11:13:04 -0400

    Add parse_and_run_program

--- python/tests/test.py
+++ python/tests/test.py
@@ -1,5 +1,17 @@
-from egg_smol.bindings import sum_as_string
+import pytest
+from egg_smol.bindings import EggSmolError, EGraph
 
 
-def test_sum_as_string():
-    assert sum_as_string(1, 2) == "3"
+class TestEGraph:
+
+    def test_parse_and_run_program(self):
+        program = "(check (= (+ 2 2) 4))"
+        egraph = EGraph()
+        assert egraph.parse_and_run_program(program) == ['Checked.']
+
+    def test_parse_and_run_program_exception(self):
+        program = "(check (= 5 4))"
+        egraph = EGraph()
+
+        with pytest.raises(EggSmolError, match='Check failed: Value { tag: "i64", bits: 5 } != Value { tag: "i64", bits: 4 }'):
+            egraph.parse_and_run_program(program)
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass(unsendable)]
+struct EGraph {
+    egraph: egg_smol::EGraph,
+}
+
+// Create exceptions with class instead of create_exception! macro
+// https://github.com/PyO3/pyo3/issues/295#issuecomment-852358088
+
+#[pyclass(extends=pyo3::exceptions::PyException)]
+pub struct EggSmolError {
+    #[pyo3(get)]
+    context: String,
+}
+
+#[pymethods]
+impl EggSmolError {
+    #[new]
+    fn new(context: String) -> Self {
+        EggSmolError { context }
+    }
+}
+#[pymethods]
+impl EGraph {
+    #[new]
+    fn new() -> Self {
+        Self {
+            egraph: egg_smol::EGraph::default(),
+        }
+    }
+
+    fn parse_and_run_program(&mut self, program: &str) -> PyResult<Vec<String>> {
+        self.egraph
+            .parse_and_run_program(program)
+            .map_err(|e| PyErr::new::<EggSmolError, _>(e.to_string()))
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn bindings(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn bindings(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<EGraph>()?;
+    m.add_class::<EggSmolError>()?;
     Ok(())
 }
