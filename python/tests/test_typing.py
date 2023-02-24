3f446a1854c021b17ecb04523361a3002d8f4d82
Saul Shanabrook <s.shanabrook@gmail.com>
2023-02-24 12:12:06 -0500

    Remove files and monkeypatch instead

--- python/tests/test_typing.py
+++ python/tests/test_typing.py
@@ -1,3 +1,25 @@
+# Monkeypatch find_test_files so that we can "remove" any files that don't really exist.
+
+# This is required using the `TypeCheckSuite`, because it calls remove on a number of builtin files.
+# we can either make empty versions of them or just monkypatch the function so that it works.
+from mypy.test import helpers
+
+original_find_test_files = helpers.find_test_files
+
+
+class ListRemoveAnything(list):
+    def remove(self, item):
+        pass
+
+
+def new_find_test_files(*args, **kwargs):
+    original_output = original_find_test_files(*args, **kwargs)
+    return ListRemoveAnything(original_output)
+
+
+helpers.find_test_files = new_find_test_files
+
+# Import TypeCheckSuite so it is picked up by pytest.
 from mypy.test.testcheck import TypeCheckSuite
 
 __all__ = ["TypeCheckSuite"]
