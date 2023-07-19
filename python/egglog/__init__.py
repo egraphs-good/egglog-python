072710f876e74a207eca6588d80ea4aef1fd6ce7
Saul Shanabrook <s.shanabrook@gmail.com>
2023-07-19 16:20:53 -0400

    Add conversion

--- python/egglog/__init__.py
+++ python/egglog/__init__.py
@@ -1,5 +1,6 @@
 from . import config, ipython_magic  # noqa: F401
 from .builtins import *  # noqa: F401
 from .egraph import *  # noqa: F401
+from .runtime import convert  # noqa: F401
 
 del ipython_magic
