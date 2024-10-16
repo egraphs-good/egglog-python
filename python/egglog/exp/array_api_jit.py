ae215ba23f4adfff5d100595934b6fa124305fa3
Saul Shanabrook <s.shanabrook@gmail.com>
2024-10-16 14:21:39 -0400

    Fix mypy

--- python/egglog/exp/array_api_jit.py
+++ python/egglog/exp/array_api_jit.py
@@ -14,8 +14,6 @@ def jit(fn: X) -> X:
     """
     Jit compiles a function
     """
-    from IPython.display import SVG
-
     # 1. Create variables for each of the two args in the functions
     sig = inspect.signature(fn)
     arg1, arg2 = sig.parameters.keys()
@@ -25,13 +23,12 @@ def jit(fn: X) -> X:
         egraph.register(res)
         egraph.run(array_api_numba_schedule)
         res_optimized = egraph.extract(res)
-        svg = SVG(egraph.graphviz_svg(split_primitive_outputs=True, n_inline_leaves=3))
+        egraph.display(split_primitive_outputs=True, n_inline_leaves=3)
 
     egraph = EGraph()
     fn_program = ndarray_function_two(res_optimized, NDArray.var(arg1), NDArray.var(arg2))
     egraph.register(fn_program)
     egraph.run(array_api_program_gen_schedule)
     fn = cast(X, egraph.eval(fn_program.py_object))
-    fn.egraph = svg  # type: ignore[attr-defined]
     fn.expr = res_optimized  # type: ignore[attr-defined]
     return fn
