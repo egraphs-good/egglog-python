61ee2eaa65766025a23b8fb03444cc56eb134c7c
Leeky <contact@bytecode.re>
2024-12-20 11:45:22 +0100

    Partial Optimization

--- python/egglog/declarations.py
+++ python/egglog/declarations.py
@@ -570,9 +570,13 @@ class CallDecl:
         return hash((self.callable, self.args, self.bound_tp_params))
 
     def __eq__(self, other: object) -> bool:
+        # Override eq to use cached hash for perf
         if not isinstance(other, CallDecl):
             return False
+        if hash(self) != hash(other):
+            return False
         return self.callable == other.callable and self.args == other.args and self.bound_tp_params == other.bound_tp_params
+
 
 
 @dataclass(frozen=True)
